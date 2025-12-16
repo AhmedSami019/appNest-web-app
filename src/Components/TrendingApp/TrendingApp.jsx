5f9693c50243ef15161cc6f0dec272c7fdab10a2
ahmedsami019 <easami019192@gmail.com>
2025-12-16 21:25:47 +0600

    added custom font

--- src/Components/TrendingApp/TrendingApp.jsx
+++ src/Components/TrendingApp/TrendingApp.jsx
@@ -17,7 +17,7 @@ const TrendingApp = () => {
 
   return (
     <div className="w-full space-y-5">
-        <h2 className="text-3xl font-extrabold">All Trending Apps </h2>
+        <h2 className="text-3xl font-bold">All Trending Apps </h2>
       <Swiper
       modules={[FreeMode]}
       spaceBetween={16} // base spacing
@@ -31,7 +31,7 @@ const TrendingApp = () => {
     >
       {trendingApp.map((app) => (
         <SwiperSlide key={app.id}>
-          <div className="card rounded-xl  overflow-hidden">
+          <div className="card rounded-xl overflow-hidden">
             {/* Image */}
             <figure className="relative h-35 lg:h-50 border-2 rounded-2xl border-gray-300">
               <img
@@ -40,7 +40,7 @@ const TrendingApp = () => {
                 className="w-full h-full object-cover"
               />
             </figure>
-            <h3 className="text-sm lg:text-lg font-bold mt-2 px-2">{app.name}</h3>
+            <h3 className="text-sm lg:text-lg font-medium mt-2 px-2">{app.name}</h3>
           </div>
         </SwiperSlide>
       ))}
