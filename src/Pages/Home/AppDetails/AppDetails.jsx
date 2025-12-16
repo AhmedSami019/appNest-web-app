78676ac82d3733a2edfdc4195afd148a5d26f73c
ahmedsami019 <easami019192@gmail.com>
2025-12-16 23:38:30 +0600

    install react icon

--- src/Pages/Home/AppDetails/AppDetails.jsx
+++ src/Pages/Home/AppDetails/AppDetails.jsx
@@ -20,10 +20,32 @@ const AppDetails = () => {
     filterNews();
   }, [id, allAppData]);
 
-  console.log(appData);
+  const {thumbnail, name, developer, downloads, category, rating, description, features, } = appData
+
+//   console.log(appData);
   return (
     <div>
-      <h2>this is app details page</h2>
+      <div className="flex gap-5">
+        <div className="w-40">
+            <img className="w-full rounded-2xl border-2 border-gray-200" src={thumbnail} alt="" />
+        </div>
+        <div className="space-y-3 py-2">
+            <h2 className="text-2xl font-semibold">{name}</h2>
+            <h4 className="text-gray-500">{category}</h4>
+            <button className="btn btn-primary">Install</button>
+        </div>
+      </div>
+      <div>
+        <div>
+            <h2>Downloaded</h2>
+            <p>{downloads}</p>
+        </div>
+        <div>
+            <h2>Ratting</h2>
+            <p>{rating}</p>
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 };
